60ed13037cf604e289541caf2721d4048f031f4b
cybermine2098 <oliverpenneltink@gmail.com>
2026-05-18 11:25:50 -0500

    I can draw boxes now!

--- phys.c
+++ phys.c
@@ -8,15 +8,20 @@
 SDL_Window* window;
 SDL_Surface* surface;
 
+//Define scales of w and h
 #define WIDTH 1600
 #define HEIGHT 900
-//Define scales of w and h
-#define YSCALE 90
-#define XSCALE 160
+#define WWIDTH 160
+#define WHEIGHT 90
 
+//Structures
 struct v2d {
   long double x;
   long double y;
+};
+struct v2di {
+  int x;
+  int y;
 };
 
 struct box{
@@ -28,20 +33,37 @@ struct box{
 
 uint32_t* pixels;
 
+//Define funcs
 struct v2d pixl(int x, int y); 
 void pixel(int x, int y, uint32_t color);
 uint32_t rgb(uint8_t r, uint8_t g, uint8_t b);
+struct v2di snap(struct v2d *mes);
+//Global Vars
+struct v2d screenScale;
+long double XSCALE;
+long double YSCALE;
+
 
 void drawBox(struct box *target){
-  struct v2d *bSize = &(target->size);
-  int pix = (int)((bSize->x * 2)*(bSize->y*2));
-  struct v2d midScreen = pix(XSCALE,YSCALE);
-  printf("pixels:%i",pix);
-  
+  struct v2d bSize = target->size;
+  struct v2d bPos = target->position;
+  struct v2d tmps = pixl(bPos.x-bSize.x,bPos.y-bSize.y);
+  struct v2d tmpe = pixl(bPos.x+bSize.x,bPos.y+bSize.y);
+  const struct v2di sPos = snap(&tmps);//Starting pixel (top left)
+  const struct v2di ePos = snap(&tmpe);//Ending pixel (bottom right)
+  for(int x = sPos.x; x < ePos.x; x++){
+    for(int y = sPos.y; y < ePos.y; y++){
+      pixel(x, y, rgb(255, 255, 255));
+    }
+  }
 }
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
+  screenScale = pixl(WIDTH, HEIGHT);
+  XSCALE = WIDTH/WWIDTH;
+  YSCALE = HEIGHT/WHEIGHT;
+  printf("Screen scaler: x:%Lf y:%Lf",screenScale.x,screenScale.y);
   window = SDL_CreateWindow("Computing Pi with colliding blocks", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
   surface = SDL_GetWindowSurface(window);
   pixels = (uint32_t*)surface->pixels;
@@ -49,14 +71,14 @@ int main() {
   //Base box
   struct box base;
   base.mass = 1;
-  base.position.x = 4.0; base.position.y = 0.0;
+  base.position.x = (WWIDTH/2)-10; base.position.y = WHEIGHT/2;
   base.size.x = 1.0;     base.size.y = 1.0;
   base.velocity.x = 0.0; base.velocity.y = 0.0;
 
   //Incoming box
   struct box initial;
   initial.mass = 1;
-  initial.position.x = 10.0; initial.position.y = 0.0;
+  initial.position.x = WWIDTH/2; initial.position.y = WHEIGHT/2;
   initial.size.x = 2.0;     initial.size.y = 2.0;
   initial.velocity.x = 0.0; initial.velocity.y = 0.0;
 
@@ -64,23 +86,23 @@ int main() {
   SDL_Event e;
   while (!quit) {
     while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT) {
-            quit = true;
-        }
-        SDL_LockSurface(surface);
-        memset(pixels, 0, sizeof(uint32_t) * WIDTH * HEIGHT);
-
-        for (int y = 0; y < 255; y++) {
-            for (int x = 0; x < 255; x++) {
-                pixel(x+100, y+100, rgb(x, 0, y));
-            }
-        }
-
-        SDL_UnlockSurface(surface);
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
     }
+
+    if (SDL_LockSurface(surface) == 0) {
+      memset(pixels, 0, sizeof(uint32_t) * WIDTH * HEIGHT);
+      //RENDER START
+      drawBox(&base);
+      drawBox(&initial);
+      //RENDER END
+      SDL_UnlockSurface(surface);
+    }
+
     SDL_UpdateWindowSurface(window);
+    SDL_Delay(16);
   }
-  
   SDL_DestroyWindow(window);
   SDL_Quit();
 }
@@ -93,8 +115,14 @@ uint32_t rgb(uint8_t r, uint8_t g, uint8_t b) {
  return (uint32_t)SDL_MapRGB(surface->format, r, g, b);
 };
 struct v2d pixl(int x, int y){
-  double xres = XSCALE/(double)WIDTH;
-  double yres = YSCALE/(double)HEIGHT;
-  struct v2d loc = {x*(1/xres),y*(1/yres)};
+  double xres = (double)WIDTH / WWIDTH;
+  double yres = (double)HEIGHT / WHEIGHT;
+  struct v2d loc = {x * xres, y * yres};
   return loc;
 };
+struct v2di snap(struct v2d *mes){
+  struct v2di re;
+  re.x = (int)mes->x;
+  re.y = (int)mes->y;
+  return re;
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# This is a project for my physics class. 
+I wouldn't reccomend looking at it too hard since I am an amateur when it comes to C.
+
+## Purpose
+As outlined in [this](https://www.youtube.com/watch?v=6dTyOl1fmDo) 3blue1brown video: you can compute increasing digits of pi using colliding blocks. I've built an interactive simulation capable of doing this. 
+
+
+
+## Included files
+- **shell.nix** -- I use nixos and this is critical for ANYTHING to EVER work on this godforsaken operating system.
+- **compile.sh** -- A simple bash script to help me speed compiling and executing up. 
+- **phys & phys.c** -- A compiled and uncompiled version of the program
+- **readme.md** -- this file
+
+### What's missing:
+- SDL Compiled folder. I can't legally include this in my git repo but you can figure out how to add it for yourself.
